099af9c741ee113a6bef95066e6b26e90dd218bb
jonathan joly <joly534@gmail.com>
2020-08-31 20:42:25 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,48 @@
-
-
-/**BOUTON D'OUVERTURE DU MENU */
-function openMenu() 
-{
-    /**on recupere les éléments */
-    var ctrlPanel = document.getElementById('control-panel');
-    var btnOpenMenu = document.getElementById('button-menu');
-    var btnCloseMenu = document.getElementById('close-menu');
-    /**LE VOLET APPARAIT */
-    ctrlPanel.style.left= 0 +'px';
-    /**LES BOUTONS CHANGENT */
-    btnOpenMenu.style.display='none';
-    btnCloseMenu.style.display='inline';
-} 
-
-/**BOUTON DE FERMETURE DU MENU */
-function closeMenu() 
-{
-    /**on recupere les éléments */
-    var ctrlPanel = document.getElementById('control-panel');
-    var btnOpenMenu = document.getElementById('button-menu');
-    var btnCloseMenu = document.getElementById('close-menu');
-    /**LE VOLET SE RETRACTE */
-    ctrlPanel.style.left= '-' + 550 +'px';
-    /**LES BOUTONS CHANGENT */
-    btnOpenMenu.style.display='inline';
-    btnCloseMenu.style.display='none';
-} 
-
-
 function main() 
 {
-    
-    
-
     /**on lance la population */
     Population();
 
-    /**on dessinne le sol */
-    drawFloor();
 
     playMusic();
+
+    var game = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    
+    function dessinerSol(sx,sy) {
+        var img = new Image();
+        img.src='assets/pictures/world/grass.png';
+        Image.onload = function() {
+            ctx.drawImage(img,sx,sy,500,500,100,100,64,64);
+        }
+    }
+
+    function dessinerGrille() {
+        for (var i=0;i<game.length;i++){
+            dessinerSol(0,0);
+        }
+    }
+    dessinerGrille();
+    console.log(game);
+    
 }
 
 main();
